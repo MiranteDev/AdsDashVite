42d039fd8f6d6daf08b659a9128f7f411cce0503
MiranteDev <41705897+MiranteDev@users.noreply.github.com>
2023-01-17 18:16:54 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AdsDashVite
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-bus6yo)
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  return <div className="App">Teste</div>;
+}
+
+export default App;
